e49bbd6ea5b97dbe60c2070502ac8cce2e88a5fe
spirizeon <spirizeon@proton.me>
2024-01-12 14:26:51 +0530

    removed unnecessary config lines and added autopairs

--- init.lua
+++ init.lua
@@ -3,78 +3,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
 vim.cmd('set number')
 
-
 local packer = require('packer')
 packer.util = require('packer.util')
-
-packer.init({
-  ensure_dependencies = true, -- Should packer install plugin dependencies?
-  snapshot = nil, -- Name of the snapshot you would like to load at startup
-  snapshot_path = packer.util.join_paths(vim.fn.stdpath('cache'), 'packer.nvim'), -- Default save directory for snapshots
-  package_root  = packer.util.join_paths(vim.fn.stdpath('data'), 'site', 'pack'),
-  compile_path  = packer.util.join_paths(vim.fn.stdpath('config'), 'plugin', 'packer_compiled.lua'),
-  plugin_package = 'packer', -- The default package for plugins
-  max_jobs = nil, -- Limit the number of simultaneous jobs. nil means no limit
-  auto_clean = true, -- During sync(), remove unused plugins
-  compile_on_sync = true, -- During sync(), run packer.compile()
-  disable_commands = false, -- Disable creating commands
-  opt_default = false, -- Default to using opt (as opposed to start) plugins
-  transitive_opt = true, -- Make dependencies of opt plugins also opt by default
-  transitive_disable = true, -- Automatically disable dependencies of disabled plugins
-  auto_reload_compiled = true, -- Automatically reload the compiled file after creating it.
-  preview_updates = false, -- If true, always preview updates before choosing which plugins to update, same as `PackerUpdate --preview`.
-  git = {
-    cmd = 'git', -- The base command for git operations
-    subcommands = { -- Format strings for git subcommands
-      update         = 'pull --ff-only --progress --rebase=false --force',
-      install        = 'clone --depth %i --no-single-branch --progress',
-      fetch          = 'fetch --depth 999999 --progress --force',
-      checkout       = 'checkout %s --',
-      update_branch  = 'merge --ff-only @{u}',
-      current_branch = 'branch --show-current',
-      diff           = 'log --color=never --pretty=format:FMT --no-show-signature HEAD@{1}...HEAD',
-      diff_fmt       = '%%h %%s (%%cr)',
-      get_rev        = 'rev-parse --short HEAD',
-      get_msg        = 'log --color=never --pretty=format:FMT --no-show-signature HEAD -n 1',
-      submodules     = 'submodule update --init --recursive --progress'
-    },
-    depth = 1, -- Git clone depth
-    clone_timeout = 60, -- Timeout, in seconds, for git clones
-    default_url_format = 'https://github.com/%s' -- Lua format string used for "aaa/bbb" style plugins
-  },
-  display = {
-    non_interactive = false, -- If true, disable display windows for all operations
-    compact = false, -- If true, fold updates results by default
-    open_fn  = nil, -- An optional function to open a window for packer's display
-    open_cmd = '65vnew \\[packer\\]', -- An optional command to open a window for packer's display
-    working_sym = '⟳', -- The symbol for a plugin being installed/updated
-    error_sym = '✗', -- The symbol for a plugin with an error in installation/updating
-    done_sym = '✓', -- The symbol for a plugin which has completed installation/updating
-    removed_sym = '-', -- The symbol for an unused plugin which was removed
-    moved_sym = '→', -- The symbol for a plugin which was moved (e.g. from opt to start)
-    header_sym = '━', -- The symbol for the header line in packer's display
-    show_all_info = true, -- Should packer show all update details automatically?
-    prompt_border = 'double', -- Border style of prompt popups.
-    keybindings = { -- Keybindings for the display window
-      quit = 'q',
-      toggle_update = 'u', -- only in preview
-      continue = 'c', -- only in preview
-      toggle_info = '<CR>',
-      diff = 'd',
-      prompt_revert = 'r',
-    }
-  },
-  luarocks = {
-    python_cmd = 'python' -- Set the python command to use for running hererocks
-  },
-  log = { level = 'warn' }, -- The default print log level. One of: "trace", "debug", "info", "warn", "error", "fatal".
-  profile = {
-    enable = false,
-    threshold = 1, -- integer in milliseconds, plugins which load faster than this won't be shown in profile output
-  },
-  autoremove = false, -- Remove disabled or unused plugins without prompting the user
-
-})
 
 packer.startup(function()
   local use = use
@@ -108,7 +38,27 @@ packer.startup(function()
   end
 }
 
-  
+use {
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!:).
+	run = "make install_jsregexp"
+}
+use {"hrsh7th/nvim-cmp"} --completion plugin
+use {"hrsh7th/cmp-buffer"} --source for text in buffer
+use {"hrsh7th/cmp-path"} --source for file system paths
+use{"saadparwaiz1/cmp_luasnip"} -- for autocompletion
+use{"rafamadriz/friendly-snippets"} -- useful snippets
+use {
+	"windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+}
+
+
+use 'andweeb/presence.nvim'
+
+
   end
 )
 
@@ -123,7 +73,7 @@ vim.keymap.set('n', '<leader>nf', vim.cmd.new, {})
 
 -- treesitter config
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "python", "javascript", "typescript", "bash", "go", "rust" },
+  ensure_installed = { "c", "lua", "vim", "python", "javascript", "typescript", "bash", "go", "rust","html","css", },
 
   sync_install = false,
   auto_install = true,
@@ -135,4 +85,45 @@ require'nvim-treesitter.configs'.setup {
 }
 
 
+-- import nvim-cmp plugin safely
+local cmp_status, cmp = pcall(require, "cmp")
+if not cmp_status then
+  return
+end
 
+-- import luasnip plugin safely
+local luasnip_status, luasnip = pcall(require, "luasnip")
+if not luasnip_status then
+  return
+end
+
+-- load VSCode-like snippets from plugins (e.g., friendly-snippets)
+
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+  }),
+  -- sources for autocompletion
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" }, -- LSP
+    { name = "luasnip" }, -- snippets
+    { name = "buffer" }, -- text within the current buffer
+    { name = "path" }, -- file system paths
+  }),
+})
