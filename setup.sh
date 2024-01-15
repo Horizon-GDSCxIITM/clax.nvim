358320b5c318ad1f97a276a4e25f46be8cffa6be
Yash Mehrotra <128923633+ReDxDaGer@users.noreply.github.com>
2024-01-15 20:11:28 +0530

    second script which help u install clax.nvim

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#Set the custom theme and update the config
+cp clax.lua ~/.local/share/nvim/site/pack/packer/start/startup.nvim/lua/startup/themes/
+echo "copied the files"
+
+echo "Now opening neovim"
+
+echo "After the installation you need to run the following command:
+	
+	nvim +PackerSync # Just press [ENTER] at any prompts
+	
+
+"
+nvim #for installing treesitter modules
