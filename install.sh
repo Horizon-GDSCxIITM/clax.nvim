1ce00072f68f50bbfe531987aba060639919a47d
spirizeon <spirizeon@proton.me>
2024-01-27 11:15:54 +0530

    added theme copy command, made install script more efficient

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
-# Makes a directory
+#!/bin/sh
 mkdir ~/.config/nvim
 
-#Clones the repository into your local machine
+#Clone dependencies
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 git clone --depth 1 https://github.com/spirizeon/clax.nvim.git
 
-#Makes a directory named clax.nvim and copy the code
+#enter directory
 cd clax.nvim
-cp init.lua ~/.config/nvim/
+
+#copy nvim config and theme
+cp init.lua ~/.config/nvim/ 
+cp clax.lua ~/.local/share/nvim/site/pack/packer/start/startup.nvim/lua/startup/themes/
 
 #Fire ups nvim (neovim)
-nvim +PackerInstall
+nvim +PackerInstall +TSUpdate
