de0ee3b7c984f5af1d5433bedf6dee08854daab4
Yash Mehrotra <128923633+ReDxDaGer@users.noreply.github.com>
2024-03-07 19:10:28 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -249,5 +249,39 @@ require('lualine').setup {
   extensions = {}
 }
 
+
+-- Import nvim-cmp plugin safely
+local cmp_status, cmp = pcall(require, "cmp")
+if not cmp_status then
+  return
+end
+
+-- Load nvim-cmp sources for HTML and CSS
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+    ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  },
+  -- Sources for autocompletion
+  sources = {
+    { name = "nvim_lsp" }, -- LSP
+    { name = "luasnip" }, -- snippets
+    { name = "buffer" }, -- text within the current buffer
+    { name = "path" }, -- file system paths
+    { name = "html" }, -- HTML completion
+    { name = "css" }, -- CSS completion
+  },
+})
+
 -- mason 
 require("mason").setup()
